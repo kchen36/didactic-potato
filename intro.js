520cb3bc46e53aad1481c5bce67b8fa9c556ec54
Jacqueline Xu <jackiestuy@gmail.com>
2017-12-08 16:59:33 -0500

    finished looping

--- intro.js
+++ intro.js
@@ -1,5 +1,6 @@
-//methods
+//================== methods ==================
 
+//
 var i = 8;
 var addToList = function(e){
 	var li = document.createElement('li');
@@ -8,33 +9,37 @@ var addToList = function(e){
 	var list = document.getElementById('thelist');
 	list.appendChild(li);
 	i++;
+	liEvents(li);
 };
 
+// changes heading to the item in the list the mouse is hovering over
 var changeHeading = function(e){
-	var heading = document.createElement('h1');
-	var node = document.createTextNode(this.innerHTML);
-	heading.appendChild(node);
 	var head = document.getElementById('h');
-	
-	head.replaceChild(heading,head.childNodes[0]);
+	head.innerHTML = this.innerHTML;
 };
 
+//changes heading back to "Hello World!" when mouse isn't hovering over anything
 var revertHeading = function(e){
 	
 };
 
+//removes item from list when mouse clicks on it
 var removeItem = function(e) {
 	
 };
 
+var liEvents = function(e) {
+	e.addEventListener('mouseover',changeHeading);
+	e.addEventListener('mouseleave',revertHeading);
+	e.addEventListener('click',removeItem);
+}
 
-// run function
+
+//================== run function ==================
 var b = document.getElementById('b');
 b.addEventListener('click',addToList);
 
 var c = document.getElementsByTagName('li');
 for (each of c) {
-	each.addEventListener('mouseover',changeHeading);
-	each.addEventListener('mouseleave',revertHeading);
-	each.addEventListener('click',removeItem);
+	liEvents(each);
 };
